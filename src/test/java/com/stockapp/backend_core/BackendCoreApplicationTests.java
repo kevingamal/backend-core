cab2efdb026c892a8881c7f6b2dc6f55175f0964
Kevin Gamal <kevingamal@gmail.com>
2026-04-30 09:53:32 -0300

    Initial commit

---
+++ src/test/java/com/stockapp/backend_core/BackendCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapp.backend_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
